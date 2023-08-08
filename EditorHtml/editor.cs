7e93d309c920229053c84daf7321472811643fd0
heyyana <anacconceicao2005@gmail.com>
2023-08-08 11:50:31 -0300

    adicionando ao repositorio

---
+++ EditorHtml/editor.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Reflection.PortableExecutable;
+using System.Text;
+
+namespace EditorHtml
+{
+    public static class Editor
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black; 
+            
+            Console.Clear();
+            
+            Console.WriteLine("Modo Editor");
+            Console.WriteLine("------------------");
+            
+            Start();
+        }
+
+        public static void Start()
+        {
+           var file = new StringBuilder();
+
+           do {
+             file.Append(Console.ReadLine());
+             file.Append(Environment.NewLine);
+             
+           } while(Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Console.WriteLine("------------------");
+            Console.WriteLine(" Deseja salvar o Arquivo?");
+
+            Viewer.Show(file.ToString());            
+        }
+    }
+}
